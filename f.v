27b3f1a86a187b03d26fd6b2c25436d317580c27
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2017-11-06 00:54:47 +0300

    Inverse fcell added

---
+++ f.v
@@ -0,0 +1,17 @@
+module f (
+    input [31:0]X,
+    input [31:0]K,
+    output [31:0]Y    
+);
+
+wire [31:0]f1 = X + K;
+wire [31:0]f2;
+
+sbox_array sba (
+    .IN(f1),
+    .OUT(f2)
+);
+
+assign Y = { f2[20:0], f2[31:21] };
+
+endmodule
